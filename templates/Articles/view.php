27969947da6af1575129cff09d2ac2709e728637
danielhrenak <daniel.hrenak@gmail.com>
2023-06-13 13:30:19 +0200

    Removed CakePHP navigation

--- templates/Articles/view.php
+++ templates/Articles/view.php
@@ -1,4 +1,3 @@
-<h1><?= h($article->title) ?></h1>
-<p><?= h($article->body) ?></p>
-<p><small>Created: <?= $article->created->format(DATE_RFC850) ?></small></p>
+<h1><?= h($article->body) ?></h1>
+<p><?= h($article->title) ?></p>
 <p><?= $this->Html->link('Edit', ['action' => 'edit', $article->slug]) ?></p>
