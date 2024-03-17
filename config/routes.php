83e01d8eab7c0e7ba5fb16151d192afd90164720
danielhrenak <daniel.hrenak@gmail.com>
2024-03-17 20:37:39 +0100

    Update routes.php

--- config/routes.php
+++ config/routes.php
@@ -78,7 +78,7 @@ return static function (RouteBuilder $routes) {
          * You can remove these routes once you've connected the
          * routes you want in your application.
          */
-//        $builder->fallbacks();
+        $builder->fallbacks();
     });
 
     /*
